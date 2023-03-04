580b3247a09c8a2556b1899f8b929c8fc6553a03
Michał Janaszek <michal.janaszek7@wp.pl>
2023-03-04 12:16:38 +0100

    Displaying Products on Index

---
+++ RecordStore.UI/Pages/Product.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.Application.Products;
+using Shop.Database;
+
+namespace RecordStore.UI.Pages
+{
+    public class ProductModel : PageModel
+    {
+        private readonly ApplicationDbContext _ctx;
+
+        public ProductModel(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public GetProduct.ProductViewModel Product { get; set; }
+
+        public IActionResult OnGet(string name)
+        {
+            Product = new GetProduct(_ctx).Do(name.Replace("-", " "));
+            if(Product == null)
+            {
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
