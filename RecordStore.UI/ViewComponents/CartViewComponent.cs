522cd58f7b0082cb7610a548985156bcfdcd9a61
Michał Janaszek <michal.janaszek7@wp.pl>
2023-04-01 16:27:50 +0200

    Cart view components

---
+++ RecordStore.UI/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Cart;
+using Shop.Database;
+
+namespace RecordStore.UI.ViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private ApplicationDbContext _ctx;
+
+        public CartViewComponent(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IViewComponentResult Invoke(string view = "Default")
+        {
+            return View(view, new GetCart(HttpContext.Session, _ctx).Do());
+        }
+    }
+}
